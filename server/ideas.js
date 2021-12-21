07a91f9f3d7a1f9d98c67a6449ad6a54860eed25
Jani Kuhno <25201137+janikuhno@users.noreply.github.com>
2021-12-21 14:03:57 +0200

    Added CRUD to idea

--- server/ideas.js
+++ server/ideas.js
@@ -2,10 +2,57 @@ const ideasRouter = require("express").Router();
 
 module.exports = ideasRouter;
 
-/* /api/ideas
- * GET /api/ideas to get an array of all ideas.
- * POST /api/ideas to create a new idea and save it to the database.
- * GET /api/ideas/:ideaId to get a single idea by id.
- * PUT /api/ideas/:ideaId to update a single idea by id.
- * DELETE /api/ideas/:ideaId to delete a single idea by id.
- */
+const {
+  getAllFromDatabase,
+  getFromDatabaseById,
+  addToDatabase,
+  updateInstanceInDatabase,
+  deleteFromDatabasebyId,
+} = require("./db");
+
+const IDEAS = "ideas";
+
+const checkMillionDollarIdea = require('./checkMillionDollarIdea');
+
+ideasRouter.param("ideaId", (req, res, next, id) => {
+  const idea = getFromDatabaseById(IDEAS, id);
+  if (idea) {
+    req.idea = idea;
+    next();
+  } else {
+    res.status(404).send();
+  }
+});
+
+/* GET /api/ideas to get an array of all ideas. */
+ideasRouter.get("/", (req, res, next) => {
+  res.send(getAllFromDatabase(IDEAS));
+});
+
+/* POST /api/ideas to create a new idea and save it to the database. */
+ideasRouter.post("/", checkMillionDollarIdea, (req, res, next) => {
+  const newIdea = addToDatabase(IDEAS, req.body);
+  res.status(201).send(newIdea);
+});
+
+/* GET /api/ideas/:ideaId to get a single idea by id. */
+ideasRouter.get("/:ideaId", (req, res, next) => {
+  res.send(req.idea);
+});
+
+/* PUT /api/ideas/:ideaId to update a single idea by id. */
+ideasRouter.put("/:ideaId", (req, res, next) => {
+  let updatedIdeaInstance = updateInstanceInDatabase(IDEAS, req.body);
+  res.send(updatedIdeaInstance);
+});
+
+/* DELETE /api/ideas/:ideaId to delete a single idea by id. */
+ideasRouter.delete("/:ideaId", (req, res, next) => {
+  const deletedIdea = deleteFromDatabasebyId(IDEAS, req.params.ideaId);
+  if (deletedIdea) {
+    res.status(204);
+  } else {
+    res.status(500);
+  }
+  res.send();
+});
