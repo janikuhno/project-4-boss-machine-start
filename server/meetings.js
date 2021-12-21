b17935027466cac9db905153a47a7b62ff8c62f8
Jani Kuhno <25201137+janikuhno@users.noreply.github.com>
2021-12-21 13:49:00 +0200

    Added get, post, delete to meetings

--- server/meetings.js
+++ server/meetings.js
@@ -4,28 +4,26 @@ module.exports = meetingsRouter;
 
 const {
   createMeeting,
-  getAllFromDatabase,
-  getFromDatabaseById,
   addToDatabase,
-  updateInstanceInDatabase,
-  deleteFromDatabasebyId,
+  getAllFromDatabase,
   deleteAllFromDatabase,
 } = require("./db");
 
 const MEETINGS = "meetings";
 
-meetingsRouter.param("meetingsId", (req, res, next, id) => {
-  const meeting = getFromDatabaseById(MEETINGS, id);
-  if (meeting) {
-    req.meeting = meeting;
-    next();
-  } else {
-    res.status(404).send();
-  }
+/* GET /api/meetings to get an array of all meetings. */
+meetingsRouter.get("/", (req, res, next) => {
+  res.send(getAllFromDatabase(MEETINGS));
 });
 
-/* GET /api/meetings to get an array of all meetings. */
-
 /* POST /api/meetings to create a new meeting and save it to the database. */
+meetingsRouter.post("/", (req, res, next) => {
+  let newMeeting = addToDatabase(MEETINGS, createMeeting());
+  res.status(201).send(newMeeting);
+});
 
 /* DELETE /api/meetings to delete all meetings from the database. */
+meetingsRouter.delete("/", (req, res, next) => {
+  deleteAllFromDatabase(MEETINGS);
+  res.status(204).send();
+});
