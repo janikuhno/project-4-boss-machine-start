223b8391740a083d9f4f412f9ea57e8d2df32439
Jani Kuhno <25201137+janikuhno@users.noreply.github.com>
2021-12-21 14:15:09 +0200

    Added work related functionality

--- server/minions.js
+++ server/minions.js
@@ -11,6 +11,7 @@ const {
 } = require("./db");
 
 const MINIONS = "minions";
+const WORK = "work";
 
 minionsRouter.param("minionId", (req, res, next, id) => {
   const minion = getFromDatabaseById(MINIONS, id);
@@ -56,6 +57,48 @@ minionsRouter.delete("/:minionId", (req, res, next) => {
 });
 
 /* GET /api/minions/:minionId/work to get an array of all work for the specified minon. */
+minionsRouter.get("/:minionId/work", (req, res, next) => {
+  const work = getAllFromDatabase(WORK).filter((singleWork) => {
+    return singleWork.minionId === req.params.minionId;
+  });
+  res.send(work);
+});
+
 /* POST /api/minions/:minionId/work to create a new work object and save it to the database. */
+minionsRouter.post("/:minionId/work", (req, res, next) => {
+  const workToAdd = req.body;
+  workToAdd.minionId = req.params.minionId;
+  const createdWork = addToDatabase(WORK, workToAdd);
+  res.status(201).send(createdWork);
+});
+
+minionsRouter.param("workId", (req, res, next, id) => {
+  const work = getFromDatabaseById(WORK, id);
+  if (work) {
+    req.work = work;
+    next();
+  } else {
+    res.status(404).send();
+  }
+});
+
 /* PUT /api/minions/:minionId/work/:workId to update a single work by id. */
+minionsRouter.put("/:minionId/work/:workId", (req, res, next) => {
+  if (req.params.minionId !== req.body.minionId) {
+    res.status(400).send();
+  } else {
+    updatedWork = updateInstanceInDatabase(WORK, req.body);
+    res.send(updatedWork);
+  }
+});
+
 /* DELETE /api/minions/:minionId/work/:workId to delete a single work by id. */
+minionsRouter.delete("/:minionId/work/:workId", (req, res, next) => {
+  const deletedWork = deleteFromDatabasebyId(WORK, req.params.workId);
+  if (deletedWork) {
+    res.status(204);
+  } else {
+    res.status(500);
+  }
+  res.send();
+});
