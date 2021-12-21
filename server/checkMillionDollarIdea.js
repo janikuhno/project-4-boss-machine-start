e0e273c599060cbd1c73470167da1a6e26d19458
Jani Kuhno <25201137+janikuhno@users.noreply.github.com>
2021-12-21 12:33:57 +0200

    Added comments for features

--- server/checkMillionDollarIdea.js
+++ server/checkMillionDollarIdea.js
@@ -1,4 +1,13 @@
 const checkMillionDollarIdea = () => {};
 
+/*
+You will create a custom middleware function checkMillionDollarIdea that will
+come in handy in some /api/ideas routes.
+Write this function in the server/checkMillionDollarIdea.js file.
+This function will make sure that any new or updated ideas are still worth at
+least one million dollars!
+The total value of an idea is the product of its numWeeks and weeklyRevenue properties.
+*/
+
 // Leave this exports assignment so that the function can be used elsewhere
 module.exports = checkMillionDollarIdea;
